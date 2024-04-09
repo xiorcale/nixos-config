460d65b113daca6584024da8bae1843c9809a9b5
xiorcale <quentin.vaucher@protonmail.com>
2024-04-09 20:11:43 +0200

    update system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -129,6 +129,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    mupen64plus
     switcheroo-control
   ];
 
